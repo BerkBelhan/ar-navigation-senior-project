7d43cc10e7fb83cf1832769eaab24a4bc7eee4c3
Berk Belhan <berk.belhan@gmail.com>
2026-01-12 20:20:16 +0300

    Updates

--- _next/static/ARnK-XJ-nNOUGJSI6a1Q1/_buildManifest.js
+++ _next/static/ARnK-XJ-nNOUGJSI6a1Q1/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ar-navigation-senior-project//_next/:path+",
+        "destination": "/ar-navigation-senior-project/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
